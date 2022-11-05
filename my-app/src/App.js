08d0f411fc19efd4b800b01a07e84bcdf6ddd988
sjchemsak <sjchemsak@gmail.com>
2022-11-05 07:55:51 -0500

    test

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          [sjc edit added 11-5-2022]
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React [sjc added this edit]
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
